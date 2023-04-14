1a577b3278c44b914ce6031970ebf6e568afe941
PedroHRdaSilva <pedroribeiro.rds@gmail.com>
2023-04-14 20:02:51 -0300

    Leitor de arquivo

--- codigo/Serie.java
+++ codigo/Serie.java
@@ -6,7 +6,7 @@ public class Serie {
     private int IdSerie;
     private String Nome, DataDeLancamento;
 
-    public Serie(int IdSerie, String Nome, String DataDeLancamento){
+    public Serie(int IdSerie, String Nome, String DataDeLancamento) {
         this.IdSerie = IdSerie;
         this.Nome = Nome;
         this.DataDeLancamento = DataDeLancamento;
