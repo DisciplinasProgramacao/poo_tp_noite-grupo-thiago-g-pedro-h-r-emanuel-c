e091a81c2eb0451aa9e299540b5d650c70aa696a
Thiago <tih02@hotmail.com>
2023-04-14 19:02:55 -0300

    Criação das classes necessárias e seus construtores. Feito por: Emanuel.

---
+++ codigo/Espectador.java
@@ -0,0 +1,11 @@
+package codigo;
+
+public class Espectador {
+    private String Nome, Login, Senha;
+
+    public Espectador(String Nome, String Login, String Senha) {
+        this.Nome = Nome;
+        this.Login = Login;
+        this.Senha = Senha;
+    }
+}
---
+++ codigo/Serie.java
@@ -0,0 +1,14 @@
+package codigo;
+
+import java.util.*;
+
+public class Serie {
+    private int IdSerie;
+    private String Nome, DataDeLancamento;
+
+    public Serie(int IdSerie, String Nome, String DataDeLancamento){
+        this.IdSerie = IdSerie;
+        this.Nome = Nome;
+        this.DataDeLancamento = DataDeLancamento;
+    }
+}
