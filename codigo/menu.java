8939fbb1ae78c67a279593bdf224d8daf2d98452
PedroHRdaSilva <pedroribeiro.rds@gmail.com>
2023-04-21 16:25:36 -0300

    Criação do hashMap , verificaLogin , lista de futura e assistida e carregar ambas

--- codigo/menu.java
+++ codigo/menu.java
@@ -2,10 +2,11 @@ package codigo;
 
 import java.io.IOException;
 
-public class Menu {
+public class menu {
     public static void main(String[] args) throws IOException {
         Plataforma plataforma = new Plataforma();
-        plataforma.infoSerie("3481");
-        
+        plataforma.infoSerie(3481);
+        plataforma.verificaLogin(null, null);
+
     }
 }
