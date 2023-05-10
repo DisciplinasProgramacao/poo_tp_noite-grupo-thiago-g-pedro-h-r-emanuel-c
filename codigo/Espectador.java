e54fe05f80b2c8bcf014396a26d664109927e855
Thiago <tih02@hotmail.com>
2023-05-09 22:13:12 -0300

    Implementação da classe filme.

--- codigo/Espectador.java
+++ codigo/Espectador.java
@@ -5,27 +5,43 @@ import java.util.List;
 
 public class Espectador {
     private String Nome, Login, Senha;
-    List<Serie> Futura;
-    List<Serie> Assistidas;
+    List<Serie> FuturaSerie;
+    List<Serie> AssistidasSerie;
+    List<Filme> FuturoFilme;
+    List<Filme> AssistidasFilme;
 
     public Espectador(String Nome, String Login, String Senha) {
         this.Nome = Nome;
         this.Login = Login;
         this.Senha = Senha;
-        this.Futura = new LinkedList<>();
-        this.Assistidas = new LinkedList<>();
+        this.FuturaSerie = new LinkedList<>();
+        this.AssistidasSerie = new LinkedList<>();
+        this.FuturoFilme = new LinkedList<>();
+        this.AssistidasFilme = new LinkedList<>();
     }
 
-    public void adicionarFutura(Serie serie) {
-        Futura.add(serie);
+    public void adicionarFuturaSerie(Serie serie) {
+        FuturaSerie.add(serie);
     }
 
-    public void adicionarAssistidas(Serie serie) {
-        Assistidas.add(serie);
+    public void adicionarAssistidasSerie(Serie serie) {
+        AssistidasSerie.add(serie);
     }
 
-    public void removerSerieFutura(Serie serie) {
-        Assistidas.remove(serie);
+    public void removerSerieFuturaSerie(Serie serie) {
+        FuturaSerie.remove(serie);
+    }
+
+    public void adicionarFuturaFilme(Filme filme) {
+        FuturoFilme.add(filme);
+    }
+
+    public void adicionarAssistidasFilme(Filme filme) {
+        AssistidasFilme.add(filme);
+    }
+
+    public void removerSerieFuturaFilme(Filme filme) {
+        FuturoFilme.remove(filme);
     }
 
     public String retornaSenha() {
