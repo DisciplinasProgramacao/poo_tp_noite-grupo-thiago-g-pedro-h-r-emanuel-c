be567719aadc1bf0f0d8ca43418909316b823839
Thiago <tih02@hotmail.com>
2023-05-23 23:27:32 -0300

    Criação da busca e começo das avaliações.

--- codigo/Midia.java
+++ codigo/Midia.java
@@ -1,32 +1,63 @@
 package codigo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Midia {
     protected int id;
-    protected String Nome;
-    protected String DataDeLancamento;
-    protected String Genero;
-    protected String Idioma;
+    protected String nome;
+    protected String dataDeLancamento;
+    protected String genero;
+    protected String idioma;
+    protected Avaliacao avaliacao;
 
-    public Midia(int id, String Nome, String DataDeLancamento, String Genero, String Idioma){
+    public enum Avaliacao {
+        NAO_AVALIADO,
+        UMA_ESTRELA,
+        DUAS_ESTRELAS,
+        TRES_ESTRELAS,
+        QUATRO_ESTRELAS,
+        CINCO_ESTRELAS
+    }
+
+    public Midia(int id, String nome, String dataDeLancamento, String genero, String idioma) {
         this.id = id;
-        this.Nome = Nome;
-        this.DataDeLancamento = DataDeLancamento;
-        this.Genero = Genero;
-        this.Idioma = Idioma;
+        this.nome = nome;
+        this.dataDeLancamento = dataDeLancamento;
+        this.genero = genero;
+        this.idioma = idioma;
+        this.avaliacao = Avaliacao.NAO_AVALIADO;
     }
 
-    abstract int retornaId();
-
-    abstract String retornaNome();
-
-    public boolean contemGeneroIdioma(String arg){
-        if(this.Genero.equals(arg)){
-            return true;
-        } else if(this.Idioma.equals(arg)){
-            return true;
-        }
-        return false;
+    public int retornaId() {
+        return id;
     }
 
-    abstract void printaMidia();
+    public String retornaNome() {
+        return nome;
+    }
+
+    public String retornaGenero() {
+        return genero;
+    }
+
+    public String retornaIdioma() {
+        return idioma;
+    }
+
+    public Avaliacao retornaAvaliacao() {
+        return avaliacao;
+    }
+
+    public void atribuirAvaliacao(Avaliacao avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void printaMidia() {
+        System.out.println("Id da Série: " + this.id + "\nNome da Série: " + this.nome + "\nData de Lançamento: " + this.dataDeLancamento + "\nAvaliação: " + avaliacao);
+    }
+
+    public String toString() {
+        return "Nome: " + nome;
+    }
 }
--- codigo/Serie.java
+++ codigo/Serie.java
@@ -6,16 +6,12 @@ public class Serie extends Midia{
         super(idSerie, Nome, DataDeLancamento, Genero, Idioma);
     }
 
-    public int retornaId() {
-        return this.id;
-    }
+    // public String retornaNome() {
+    //     return this.Nome;
+    // }
 
-    public String retornaNome() {
-        return this.Nome;
-    }
-
-    public void printaMidia() {
-        System.out.println("Id da Série: " + this.id + "\nNome da Série: " + this.Nome + "\nData de Lançamento: "
-                + this.DataDeLancamento);
-    }
+    // public void printaMidia() {
+    //     System.out.println("Id da Série: " + this.id + "\nNome da Série: " + this.Nome + "\nData de Lançamento: "
+    //             + this.DataDeLancamento);
+    // }
 }
