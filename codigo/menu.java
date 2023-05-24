d3697c9b1a42f9363eb598404935e68ddc087883
EmanuelCostaa <enmc0104@gmail.com>
2023-05-23 23:48:58 -0300

    teste2 Emanuel

--- codigo/menu.java
+++ codigo/menu.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class menu {
     public static void main(String[] args) throws IOException {
         Plataforma plataforma = new Plataforma();
-        // System.out.println(plataforma.buscaIdiomaMidia("Inglês"));
+        System.out.println(plataforma.buscaIdiomaMidia("Inglês"));
         System.out.println(plataforma.buscaGeneroMidia("Drama"));
         plataforma.adicionarAvaliacao(3509, Midia.Avaliacao.UMA_ESTRELA);
         plataforma.adicionarAvaliacao(3531, Midia.Avaliacao.UMA_ESTRELA);
