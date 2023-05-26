18811330e521767a1f396d3b2bd70b413b8b0422
Thiago <tih02@hotmail.com>
2023-05-26 20:58:16 -0300

    Súbida Enums e correção arquivos.

---
+++ codigo/EGeneros.java
@@ -0,0 +1,15 @@
+package codigo;
+
+public class EGeneros {
+    public enum Genero{
+        ACAO,
+        ANIME,
+        AVENTURA,
+        COMEDIA,
+        DOCUMENTARIO,
+        DRAMA,
+        POLICIAL,
+        ROMANCE,
+        SUSPENSE
+    }
+}
---
+++ codigo/EIdioma.java
@@ -0,0 +1,15 @@
+package codigo;
+
+public class EIdioma {
+    public enum Idioma{
+        INGLES,
+        CHINES,
+        HINDI,
+        ESPANHOL,
+        FRANCES,
+        ARABE,
+        BENGALI,
+        RUSSO,
+        PORTUGUES
+    }
+}
