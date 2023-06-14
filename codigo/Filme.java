b0e6c0b94d860f86b973413f5799bd57ef8f367d
Thiago <tih02@hotmail.com>
2023-06-13 23:15:34 -0300

    Ajustes necessários no menu e adicionado lançamento.

--- codigo/Filme.java
+++ codigo/Filme.java
@@ -3,8 +3,8 @@ package codigo;
 public class Filme extends Midia{
     private int DuracaoMin;
 
-    public Filme(int idFilme, String Nome, String DataDeLancamento, int DuracaoMin, String Genero, String Idioma) {
-        super(idFilme, Nome, DataDeLancamento, Genero, Idioma);
+    public Filme(int idFilme, String Nome, String DataDeLancamento, int DuracaoMin, String Genero, String Idioma, boolean Lancamento) {
+        super(idFilme, Nome, DataDeLancamento, Genero, Idioma, Lancamento);
         this.DuracaoMin = DuracaoMin;
     }
 }
