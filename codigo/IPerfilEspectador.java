fad496fcd069590db8c2fc5d6852fdc3afd2f3f3
Thiago <tih02@hotmail.com>
2023-06-16 18:27:13 -0300

    Documentação interface.

--- codigo/IPerfilEspectador.java
+++ codigo/IPerfilEspectador.java
@@ -1,6 +1,9 @@
 package codigo;
 
 public interface IPerfilEspectador {
+    /**
+     * Interface do Perfil Espectador.
+     */
     public boolean podeComentar();
     public String retornaTipo();
     public boolean podeAssistirLancamento();
