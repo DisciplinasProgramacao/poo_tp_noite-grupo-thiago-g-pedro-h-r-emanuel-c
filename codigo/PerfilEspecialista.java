55cfa152ed3989d47f6f7a5ec9eec5771c259fe7
Thiago <tih02@hotmail.com>
2023-06-16 18:35:51 -0300

    Documentado todos os perfis.

--- codigo/PerfilEspecialista.java
+++ codigo/PerfilEspecialista.java
@@ -1,24 +1,60 @@
 package codigo;
 
+/**
+ * A classe PerfilEspecialista implementa a interface IPerfilEspectador e define
+ * um perfil de espectador especialista.
+ */
 public class PerfilEspecialista implements IPerfilEspectador {
-    final private String tipo = "Especialista";
-    final private boolean podeComentar = true;
-    final private boolean podeAssistirLancamento = false;
 
-    public PerfilEspecialista(){}
+    /**
+     * O tipo de perfil do espectador.
+     */
+    private final String tipo = "Especialista";
 
+    /**
+     * Indica se o espectador especialista pode comentar.
+     */
+    private final boolean podeComentar = true;
+
+    /**
+     * Indica se o espectador especialista pode assistir a lançamentos.
+     */
+    private final boolean podeAssistirLancamento = false;
+
+    /**
+     * Construtor padrão da classe PerfilEspecialista.
+     */
+    public PerfilEspecialista() {
+    }
+
+    /**
+     * Verifica se o espectador especialista pode comentar.
+     * 
+     * @return true se o espectador pode comentar, false caso contrário.
+     */
     @Override
     public boolean podeComentar() {
         return this.podeComentar;
     }
 
+    /**
+     * Retorna o tipo de perfil do espectador.
+     * 
+     * @return o tipo de perfil do espectador.
+     */
     @Override
     public String retornaTipo() {
         return this.tipo;
     }
 
+    /**
+     * Verifica se o espectador especialista pode assistir a lançamentos.
+     * 
+     * @return true se o espectador pode assistir a lançamentos, false caso
+     *         contrário.
+     */
     @Override
-    public boolean podeAssistirLancamento(){
+    public boolean podeAssistirLancamento() {
         return this.podeAssistirLancamento;
     }
 }
--- codigo/PerfilRegular.java
+++ codigo/PerfilRegular.java
@@ -1,25 +1,54 @@
 package codigo;
 
+/**
+ * A classe PerfilRegular implementa a interface IPerfilEspectador e define um perfil de espectador regular.
+ */
 public class PerfilRegular implements IPerfilEspectador {
-    final private String tipo = "Regular";
-    final private boolean podeComentar = false;
-    final private boolean podeAssistirLancamento = false;
+    
+    /**
+     * O tipo de perfil do espectador.
+     */
+    private final String tipo = "Regular";
+    
+    /**
+     * Indica se o espectador regular pode comentar.
+     */
+    private final boolean podeComentar = false;
+    
+    /**
+     * Indica se o espectador regular pode assistir a lançamentos.
+     */
+    private final boolean podeAssistirLancamento = false;
 
-    public PerfilRegular(){}
+    /**
+     * Construtor padrão da classe PerfilRegular.
+     */
+    public PerfilRegular() {}
 
+    /**
+     * Verifica se o espectador regular pode comentar.
+     * @return true se o espectador pode comentar, false caso contrário.
+     */
     @Override
     public boolean podeComentar() {
         return this.podeComentar;
     }
 
+    /**
+     * Retorna o tipo de perfil do espectador.
+     * @return o tipo de perfil do espectador.
+     */
     @Override
     public String retornaTipo() {
         return this.tipo;
     }
 
+    /**
+     * Verifica se o espectador regular pode assistir a lançamentos.
+     * @return true se o espectador pode assistir a lançamentos, false caso contrário.
+     */
     @Override
-    public boolean podeAssistirLancamento(){
+    public boolean podeAssistirLancamento() {
         return this.podeAssistirLancamento;
     }
-    
 }
